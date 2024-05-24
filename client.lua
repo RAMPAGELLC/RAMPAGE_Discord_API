ffe21f046f2413c97caff023df31701c22dfacfd
jake <53842001+vq9o@users.noreply.github.com>
2024-05-24 10:56:35 -0700

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,8 +1,16 @@
+-- Copyright (c) 2024 RAMPAGE Interactive. All rights reserved.
+-- Re-write of Badger_Discord_API to be better organized & additional features.
+
 local triggeredServer = false;
+
 AddEventHandler("playerSpawned", function()
     if not triggeredServer then 
         triggeredServer = true;
         Citizen.Wait(12 * 1000)
         TriggerServerEvent('RAMPAGE_Discord_API:LoadPlayer');
     end
+end)
+
+RegisterCommand('refreshdiscord', function()
+    TriggerServerEvent('RAMPAGE_Discord_API:LoadPlayer');
 end)
